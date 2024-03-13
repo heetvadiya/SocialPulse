b4c044b8d7c29efd353a2cf0bf7d83b3e1e0d063
heetvadiya <heetvadiya@gmail.com>
2024-03-14 01:36:34 +0530

    changed middleware.ts for deployment

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,31 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
+  publicRoutes: [
+    "/",
+    "/sign-in(.*)",
+    "/sign-up(.*)",
+    "/api(.*)",
+  ],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //  For public routes, we don't need to do anything
+      return NextResponse.next();
+    }
+
+    const url = new URL(req.nextUrl.origin);
+
+    if (!auth.userId) {
+      //  If user tries to access a private route without being authenticated,
+      //  redirect them to the sign in page
+      url.pathname = "/sign-in";
+      return NextResponse.redirect(url);
+    }
+  },
 });
  
 export const config = {
